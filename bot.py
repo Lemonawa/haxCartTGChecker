629549ae530da86f930c9096e102c4e6dc20ae2e
Misaka No <96560028+misakano7545@users.noreply.github.com>
2021-12-30 22:42:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,20 +43,25 @@ def getWebpage():
     allvps = allVPS[15].string.replace(' VPS', '')
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="您好，欢迎使用Hax库存查询监控bot！\n我能够帮你拿到hax官网上的库存信息，并把他们发送到你的Telegram会话中\nGithub: @isaka-blog    TG: @misakanetcn")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="欢迎使用Hax库存查询监控bot！\n我能够帮你拿到hax官网上的库存信息，并把他们发送到你的Telegram会话中\n输入 /help 获取帮助列表\nGithub: Misaka-blog    TG: @misakanetcn")
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hax 库存查询监控BOT 帮助菜单\n/help 显示本菜单\n/get 获取当前库存情况\n/ping 检测bot存活状态")
 
 def ping(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Pong~")
 
 def get(update, context):
     getWebpage()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前的库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nUS-LA: "+usla+"\nUS-NYC: "+usnyc+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+allvps+"\nNote: EU1-EU10、EU-Mid-1 为KVM区，ID-1、US-LA和US-NYC为LXC区")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前的库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nUS-LA: "+usla+"\nUS-NYC: "+usnyc+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+allvps+"\nNote: EU1-EU10、EU-Mid-1 为KVM区，ID-1、US-LA和US-NYC为LXC区；US-NYC区硬盘为HDD")
 
 Start = CommandHandler('start', start)
 Ping = CommandHandler('ping', ping)
 Get = CommandHandler('get', get)
+Help = CommandHandler('help', help)
 dispatcher.add_handler(Ping)
 dispatcher.add_handler(Start)
 dispatcher.add_handler(Get)
+dispatcher.add_handler(Help)
 
 updater.start_polling()
