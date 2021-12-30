21bc6baf04a62f402c38d9ef6122bcfd267c7ea7
Misaka No <96560028+misakano7545@users.noreply.github.com>
2021-12-30 17:43:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,10 +36,11 @@ def getWebpage():
     eu9 = allVPS[8].string.replace(' VPS', '')
     eu10 = allVPS[9].string.replace(' VPS', '')
     eumid1 = allVPS[10].string.replace(' VPS', '')
-    id1 = allVPS[11].string.replace(' VPS', '')
-    usla = allVPS[12].string.replace(' VPS', '')
-    euovz = allVPS[13].string.replace(' VPS', '')
-    all = allVPS[14].string.replace(' VPS', '')
+    usnyc = allVPS[11].string.replace(' VPS', '')
+    id1 = allVPS[12].string.replace(' VPS', '')
+    usla = allVPS[13].string.replace(' VPS', '')
+    euovz = allVPS[14].string.replace(' VPS', '')
+    all = allVPS[15].string.replace(' VPS', '')
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="您好，欢迎使用Hax库存查询监控bot！\n我能够帮你拿到hax官网上的库存信息，并把他们发送到你的Telegram会话中\nGithub: @isaka-blog    TG: @misakanetcn")
@@ -49,7 +50,7 @@ def ping(update, context):
 
 def get(update, context):
     getWebpage()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前的库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nUS-LA: "+usla+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+all+"\nNote: EU1-EU10、EU-Mid-1 为KVM区，ID-1 和 US-LA 为LXC区")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前的库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nUS-LA: "+usla+"\nUS-NYC: "+usnyc+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+all+"\nNote: EU1-EU10、EU-Mid-1 为KVM区，ID-1、US-LA和US-NYC为LXC区")
 
 Start = CommandHandler('start', start)
 Ping = CommandHandler('ping', ping)
