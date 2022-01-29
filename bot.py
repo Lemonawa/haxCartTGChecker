b956889775cbd84b0e753c7a3b21b6d9c94cd0ab
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-29 18:51:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def getSoup(url,encoding="utf-8",**params):
     return soup
 
 def getWebpage():
-    global eu1,eu2,eu3,eu4,eu5,eu6,eu7,eu8,eu9,eu10,eumid1,euovz,id1,usla2,allvps
+    global eu1,eu2,eu3,eu4,eu5,eu6,eu7,eu8,eu9,eu10,eumid1,euovz,id1,sg1,allvps
     params = getParams()
     soup = getSoup("https://hax.co.id/data-center/",**params)
     allVPS = soup.find_all('h1')
@@ -37,7 +37,7 @@ def getWebpage():
     eu10 = allVPS[9].string.replace(' VPS', '')
     eumid1 = allVPS[10].string.replace(' VPS', '')
     id1 = allVPS[11].string.replace(' VPS', '')
-    usla2 = allVPS[12].string.replace(' VPS', '')
+    sg1 = allVPS[12].string.replace(' VPS', '')
     euovz = allVPS[13].string.replace(' VPS', '')
     allvps = allVPS[14].string.replace(' VPS', '')
 
@@ -52,7 +52,7 @@ def ping(update, context):
 
 def get(update, context):
     getWebpage()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nUS-LA2: "+usla2+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+allvps+"\nNote: EU1-EU10、EU-Mid-1为KVM区，ID-1、US-LA2为LXC区")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="我获取到网页了！下面是Hax VPS目前库存情况：\nEU-1: "+eu1+"\nEU-2: "+eu2+"\nEU-3: "+eu3+"\nEU-4: "+eu4+"\nEU-5: "+eu5+"\nEU-6: "+eu6+"\nEU-7: "+eu7+"\nEU-8: "+eu8+"\nEU-9: "+eu9+"\nEU-10: "+eu10+"\nEU-Mid-1: "+eumid1+"\nID-1: "+id1+"\nSG1: "+sg1+"\nEU-OpenVZ: "+euovz+"\n开通的VPS: "+allvps+"\nNote: EU1-EU10、EU-Mid-1为KVM区，ID-1、US-LA2为LXC区")
 
 Start = CommandHandler('start', start)
 Ping = CommandHandler('ping', ping)
